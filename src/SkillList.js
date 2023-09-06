c07d27439416140c4bd2b942b6404b1440decfd4
hell0worId <arzookharayat@gmail.com>
2023-09-06 11:06:45 +0530

    third commit

--- src/SkillList.js
+++ src/SkillList.js
@@ -1,12 +1,50 @@
-import Skill from "./Skill";
 export default function SkillList() {
+  const skills = [
+    {
+      skill: "React",
+      level: "beginner",
+      color: "blue",
+    },
+    {
+      skill: "HTML",
+      level: "advanced",
+      color: "orange",
+    },
+    {
+      skill: "CSS",
+      level: "beginner",
+      color: "orangered",
+    },
+    {
+      skill: "Node.js",
+      level: "beginner",
+      color: "green",
+    },
+    {
+      skill: "JavaScript",
+      level: "advanced",
+      color: "yellow",
+    },
+    {
+      skill: "Git and Github",
+      level: "intermediate",
+      color: "lightblue",
+    },
+  ];
+
+  const levels = {
+    beginner: "👶",
+    intermediate: "👍",
+    advanced: "💪",
+  };
   return (
     <div className="skill-list">
-      <Skill skill="React" emoji="👶" color="blue"></Skill>
-      <Skill skill="HTML" emoji="💪" color="orange"></Skill>
-      <Skill skill="CSS" emoji="👶" color="orangered"></Skill>
-      <Skill skill="Node.js" emoji="👶" color="green" />
-      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      {skills.map((skill) => (
+        <div className="skill" style={{ backgroundColor: skill.color }}>
+          <span>{skill.skill}</span>
+          <span>{levels[skill.level]}</span>
+        </div>
+      ))}
     </div>
   );
 }
